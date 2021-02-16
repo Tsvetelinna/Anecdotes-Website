862b8b08e9ab3629da7d13134dc44807d8eafde3
Tsvetelinna <cvetilinna@gmail.com>
2021-02-16 13:22:17 +0200

    Add project structure

---
+++ anecdotes/src/main/java/course/spring/AnecdotesApplication.java
@@ -0,0 +1,12 @@
+package course.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnecdotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnecdotesApplication.class, args);
+    }
+}
